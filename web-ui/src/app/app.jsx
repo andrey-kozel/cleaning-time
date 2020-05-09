a03a81813b060402f6982ae5485bddfed506a832
Andrey Kozel <andrey.kozel@kaseya.com>
2020-05-09 15:20:25 +0300

    Login and logout templates moved closer to the place of the usage

--- web-ui/src/app/app.jsx
+++ web-ui/src/app/app.jsx
@@ -2,7 +2,7 @@ import React, {Fragment} from 'react'
 import {CssBaseline} from "@material-ui/core";
 import {Route, Switch} from 'react-router-dom'
 
-import PrivateRoute from "../common/components/auth/components/private-route";
+import PrivateRoute from "../common/components/private-route";
 
 import TokenRefresher from '../features/components/token-refresher'
 import LoginPage from "../features/components/login-page";
