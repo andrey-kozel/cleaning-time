28e5d64b19307d7de5b9f81662180bfc01c4760a
Andrey Kozel <andrey.kozel@kaseya.com>
2020-10-31 16:49:55 +0300

    Updated versions of java libraries

--- rest-client/src/main/java/dev/akozel/cleaningtime/rest/swagger/SwaggerConfiguration.java
+++ rest-client/src/main/java/dev/akozel/cleaningtime/rest/swagger/SwaggerConfiguration.java
@@ -1,7 +1,5 @@
 package dev.akozel.cleaningtime.rest.swagger;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -12,11 +10,11 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * SwaggerConfiguration. Swagger configuration
@@ -26,7 +24,6 @@ import java.util.List;
  * @author Andrey Kozel
  */
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
 
     @Bean
@@ -57,7 +54,7 @@ public class SwaggerConfiguration {
                 = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return Lists.newArrayList(
+        return Collections.singletonList(
                 new SecurityReference("AUTHORIZATION", authorizationScopes));
     }
 
@@ -66,7 +63,7 @@ public class SwaggerConfiguration {
         private static final List<String> PUBLIC_URL_REQUEST = Arrays.asList("/v1/auth", "/v1/registrations");
 
         @Override
-        public boolean apply(String url) {
+        public boolean test(String url) {
             return !PUBLIC_URL_REQUEST
                     .stream()
                     .anyMatch(url::equals);
